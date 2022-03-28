ee82abdca7db0f4666ae19a4cf3c7a7efbc09b13
Alex <alexferrara74@gmail.com>
2022-03-28 11:14:14 +0200

    a

---
+++ my-app/src/App.js
@@ -0,0 +1,56 @@
+
+import React, { Component, useRef } from 'react';
+import './css/grafica.css'
+import Navbar from './components/navbar';
+import Sfondo from './components/sfondo';
+import sfondo from './img/sfondo.jpg'
+import Card from './components/card';
+
+class App extends Component {
+  state= {cards:[
+    
+  ]}
+   
+
+    aggiungi=()=>{
+      const cards=[{id:0, testo:"bracciale" , foto:sfondo},
+      {id:1, testo:"bracciale" , foto:sfondo},
+      {id:2, testo:"bracciale" , foto:sfondo},
+      {id:3, testo:"bracciale" , foto:sfondo}]
+      this.setState({cards});
+      
+    }
+   
+
+
+  render(){
+  return (
+    <>
+    
+      <Navbar 
+      agg={this.aggiungi}/>
+      <Sfondo valore={sfondo} />
+  
+    <div id='card-container'>
+      {this.state.cards.map(card=> (
+        <Card 
+        key={card.id}
+        testo={card.testo}
+        foto={card.foto}
+        />
+      ))}
+    </div>
+        <div id='secondapagina' >
+          
+
+        </div>
+
+
+
+    </>
+  );
+  }
+}
+
+
+export default App;
