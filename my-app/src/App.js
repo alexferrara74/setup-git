e8f746af1fe4545208cd1ab9dfb643f829a332be
Alex <alexferrara74@gmail.com>
2022-04-01 07:09:34 +0200

    a

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,22 @@ import Sfondo from './components/sfondo';
 import sfondo from './img/sfondo.jpg'
 import Card from './components/card';
 
-class App extends Component {
-  state= {cards:[
-    
-  ]}
-   
 
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.secondapagina = React.createRef();
+    this.terzapagina = React.createRef();
+    this.quartapagina = React.createRef();
+    this.primapagina = React.createRef();
+  }
+  pagina1=()=>this.primapagina.current.scrollIntoView({behavior:"smooth"});
+  pagina2=()=>this.secondapagina.current.scrollIntoView({behavior:"smooth"});
+  pagina3=()=>this.terzapagina.current.scrollIntoView({behavior:"smooth"});
+  pagina4=()=>this.quartapagina.current.scrollIntoView({behavior:"smooth"});
+
+  state= {cards:[]}
     aggiungi=()=>{
      
       const cards=[{id:0, testo:"bracciale" , foto:sfondo},
@@ -26,18 +36,21 @@ class App extends Component {
       const cards=[];
       this.setState({cards});    }
 
+    
 
   render(){
   return (
     <>
-    
+   
       <Navbar 
+      scrolla2={this.pagina2}
+      scrolla3={this.pagina3}
+      scrolla4={this.pagina4}
       agg={this.aggiungi}
       remove={this.remove}/>
       <Sfondo valore={sfondo} />
-  
+    
     <div id='card-container'>
-      
       {this.state.cards.map(card=> (
         <Card 
         key={card.id}
@@ -46,9 +59,27 @@ class App extends Component {
         />
       ))}
     </div>
-        <div id='secondapagina' >
-          
+        <div id='secondapagina' ref={this.secondapagina}>
+        <Navbar
+        scrolla1={this.pagina1}
+        scrolla3={this.pagina3}
+        scrolla4={this.pagina4}/>
+        <Sfondo valore={sfondo} />
+        </div>
+        <div id='terzapagina' ref={this.terzapagina}>
+        <Navbar
+        scrolla1={this.pagina1}
+        scrolla2={this.pagina2}
+        scrolla4={this.pagina4} />
+        <Sfondo valore={sfondo} />
+        </div>
 
+        <div id='quartapagina' ref={this.quartapagina}>
+        <Navbar
+        scrolla1={this.pagina1}
+        scrolla2={this.pagina2}
+        scrolla3={this.pagina3}/>
+        <Sfondo valore={sfondo} />
         </div>
 
 
--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,20 +1,22 @@
 import React, {Component} from 'react';
 import '../css/grafica.css'
 import valore from '../img/free.png'
-import Card from './card';
+
 class Navbar extends Component{
+        
     render(){
     return(
-        <div className='animate__animated animate__bounce animate__delay-2s' id="barra">
+            
+        <div id="barra">
                 <div id="logo">
                         <img id='imglogo' src={valore} alt=""/>
                 </div>
                 <ul id="navbar"> 
                 
-                    <li class="pulsante" onDoubleClick={()=>this.props.remove()} onClick={()=>this.props.agg()} >Home</li>
-                    <li class="pulsante" >Prodotti</li>
-                    <li class="pulsante">Tutorial</li>
-                    <li class="pulsante">Chi Sono</li>
+                    <li class="pulsante" onClick={()=>this.props.scrolla1()}>Home</li>
+                    <li class="pulsante" onClick={()=>this.props.scrolla2()}>Prodotti</li>
+                    <li class="pulsante" onClick={()=>this.props.scrolla3()}>Tutorial</li>
+                    <li class="pulsante" onClick={()=>this.props.scrolla4()}>Chi Sono</li>
                  </ul>
         </div>
         );
